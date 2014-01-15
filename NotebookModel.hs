fb15a575dbb8f55d48374a8259df2e79114a8c2c
kjrz <kuba.jarz@gmail.com>
2014-01-15 23:11:46 +0100

    added marking entries as done and persistance

--- NotebookModel.hs
+++ NotebookModel.hs
@@ -4,12 +4,21 @@ module NotebookModel
   (
     Entry(..)
   , Notebook
+  , addEntry
+  , removeEntry
+  , markAsDone
+  , saveToFile
+  , loadFromFile
   ) where
 
+import qualified Data.ByteString.Lazy.Char8 as BL
 import Control.Applicative ((<$>), (<*>), empty)
 import Data.Aeson
+import Data.Maybe (fromJust)
 
 import EventModel
+
+-- Entry in the notebook
 
 data Entry = Entry { event :: Event
                    , done  :: Bool
@@ -25,4 +34,35 @@ instance FromJSON Entry where
                          v .: "done"
   parseJSON _          = empty
 
+-- Notebook full of entries
+
 type Notebook = [Entry]
+
+addEntry :: Entry -> Notebook -> Notebook
+addEntry e n = e : n
+
+removeEntry :: Int -> Notebook -> Notebook
+removeEntry _ []     = []
+removeEntry 1 (_:xs) = xs
+removeEntry n (x:xs) = x : removeEntry (n-1) xs
+
+markAsDone :: Int -> Notebook -> Notebook
+markAsDone _ []     = []
+-- markAsDone 1 (x:xs) = nextOccurence x : xs
+markAsDone 1 (x:xs) | (recurrence . event) x == None
+                      = xs
+                    | otherwise
+                      = Entry ((nextOccurence . event) x)
+                              False
+                        : xs
+markAsDone n (x:xs) = x : markAsDone (n-1) xs
+
+-- Persistance
+
+saveToFile :: Notebook -> IO ()
+saveToFile n = BL.writeFile "notebook.json" $ encode n
+
+loadFromFile :: IO Notebook
+loadFromFile = do
+  notebookFromFile <- BL.readFile "notebook.json"
+  return $ fromJust $ decode notebookFromFile
--- NotebookModelTest.hs
+++ NotebookModelTest.hs
@@ -10,37 +10,71 @@ import NotebookModel
 main :: IO ()
 main = do
 
-	-- Events
-	let firstEvent = Event "w" (eventDateTime (2000,3,3)
-							   (22,3,0)) Monthly
-	let secondEvent = Event "y" (eventDateTime (2001,1,1)
-								(23,7,0)) Monthly
+  -- Events
+  let firstEvent = Event "w" (eventDateTime (2000,3,3)
+                 (22,3,0)) Monthly
+  let secondEvent = Event "y" (eventDateTime (2001,1,1)
+                (23,7,0)) Monthly
 
-	-- Entry
-	let firstEntry = Entry firstEvent False
-	BL.writeFile "kuba-entry.json" $ encode firstEntry
-	let secondEntry = Entry secondEvent False
-	BL.writeFile "kuba-entry.json" $ encode secondEntry
-	entryFromFile <- BL.readFile "kuba-entry.json"
-	putStrLn ""
-	putStrLn "-- entryFromFile:"
-	BL.putStrLn entryFromFile
-	let decodedEntryFromFile = decode entryFromFile :: Maybe Entry
-	putStrLn ""
-	putStrLn "-- decodedEntryFromFile:"
-	print $ fromJust decodedEntryFromFile
+  -- Entry
+  let firstEntry = Entry firstEvent False
+  BL.writeFile "kuba-entry.json" $ encode firstEntry
+  let secondEntry = Entry secondEvent False
+  BL.writeFile "kuba-entry.json" $ encode secondEntry
+  entryFromFile <- BL.readFile "kuba-entry.json"
+  putStrLn ""
+  putStrLn "-- entryFromFile:"
+  BL.putStrLn entryFromFile
+  let decodedEntryFromFile = decode entryFromFile :: Maybe Entry
+  putStrLn ""
+  putStrLn "-- decodedEntryFromFile:"
+  print $ fromJust decodedEntryFromFile
 
-	-- Notebook
-	let notebook = [firstEntry, secondEntry] :: Notebook
-	BL.writeFile "kuba-notebook.json" $ encode notebook
-	notebookFromFile <- BL.readFile "kuba-notebook.json"
-	putStrLn ""
-	putStrLn "-- notebookFromFile:"
-	BL.putStrLn notebookFromFile
-	let decodedNotebookFromFile = decode notebookFromFile :: Maybe Notebook
-	putStrLn ""
-	putStrLn "-- decodedNotebookFromFile:"
-	print $ fromJust decodedNotebookFromFile
+  -- Notebook
+  let notebook = [firstEntry, secondEntry] :: Notebook
+  BL.writeFile "kuba-notebook.json" $ encode notebook
+  notebookFromFile <- BL.readFile "kuba-notebook.json"
+  putStrLn ""
+  putStrLn "-- notebookFromFile:"
+  BL.putStrLn notebookFromFile
+  let decodedNotebookFromFile = decode notebookFromFile :: Maybe Notebook
+  putStrLn ""
+  putStrLn "-- decodedNotebookFromFile:"
+  print $ fromJust decodedNotebookFromFile
 
-	putStrLn ""
-	putStrLn "-- ok"
+  -- add Entry to Notebook
+  putStrLn ""
+  putStrLn "-- extendedNotebook:"
+  let extendedNotebook = secondEntry : notebook :: Notebook
+  print extendedNotebook
+
+  -- remove Entry from Notebook: wrong n's
+  putStrLn ""
+  putStrLn "-- croppedNotebooks:"
+  let croppedNotebookOne = removeEntry 6 extendedNotebook
+  print croppedNotebookOne
+  let croppedNotebookTwo = removeEntry 0 extendedNotebook
+  print croppedNotebookTwo
+
+  -- remove Entry from Notebook: ok n
+  let croppedNotebookThree = removeEntry 3 extendedNotebook
+  print croppedNotebookThree
+
+  -- mark as done
+  putStrLn ""
+  putStrLn "-- dondoneNotebook:"
+  let dondoneNotebook = markAsDone 1 extendedNotebook
+  print dondoneNotebook
+
+  -- save to file
+  putStrLn ""
+  putStrLn "-- saving to file..."
+  saveToFile dondoneNotebook
+
+  -- load from file
+  putStrLn ""
+  putStrLn "-- loaded from file"
+  loadedFromFile <- loadFromFile :: IO Notebook
+  print loadedFromFile
+
+  putStrLn "-- ok"
