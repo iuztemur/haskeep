6fd550bf1c2e1e9f82912521f71edf914bafd856
kjrz <kuba.jarz@gmail.com>
2014-01-17 15:52:17 +0100

    printing/selective printing notebook entries

---
+++ NotebookPrinterTest.hs
@@ -0,0 +1,22 @@
+import NotebookModel
+import NotebookPrinter
+import EventModel
+
+main :: IO ()
+main = do
+
+  -- Events, entries, notebook
+  let firstDate   = eventDateTime (2000,3,3) (22,3,0)
+  let firstEvent  = Event "w" firstDate Monthly
+  let secondEvent = Event "y" (eventDateTime (2001,1,1)
+                    (23,7,0)) Monthly
+  let firstEntry  = Entry firstEvent False
+  let secondEntry = Entry secondEvent True
+  let notebook = [firstEntry, secondEntry] :: Notebook
+
+  -- Print!
+  printNotebook notebook
+  printNotebookFiltered (isToday firstDate) notebook
+  printNotebookFiltered (isDone True) notebook
+
+  putStrLn "-- ok"
