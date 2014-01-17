e1cd146f8cf422488e73ada22d30a2d6bf3dc0d6
kjrz <kuba.jarz@gmail.com>
2014-01-17 15:52:42 +0100

    printing/selective printing notebook entries

---
+++ NotebookPrinter.hs
@@ -0,0 +1,55 @@
+module NotebookPrinter
+  (
+    printNotebook
+  , printNotebookFiltered
+  ) where
+
+import NotebookModel
+import EventModel
+
+printDoneUndone :: Entry -> String
+printDoneUndone e | done e    = "DONE"
+                  | otherwise = "TODO"
+
+printNotebookHeader :: IO ()
+printNotebookHeader = putStrLn $
+                      "No :: Name :: Date & time"
+                      ++ " :: DONE/TODO "
+
+
+printEntry :: Int -> Entry -> IO ()
+printEntry n e = putStrLn $
+                  show n ++ " :: " ++
+                  (name . event) e ++ " :: " ++
+                  show ( (dateTime . event) e) ++
+                  " :: " ++ printDoneUndone e
+
+printNotebookIterate :: Int -> Notebook -> IO ()
+printNotebookIterate _ []     = putStrLn ""
+printNotebookIterate 0 xs     = do
+                                  printNotebookHeader
+                                  printNotebookIterate 1 xs
+printNotebookIterate n (x:xs) = do
+                                  printEntry n x
+                                  printNotebookIterate (n+1) xs
+
+printNotebook :: Notebook -> IO ()
+printNotebook = printNotebookIterate 0
+
+printNotebookFilteredIterate :: Int
+                                -> (Entry -> Bool)
+                                -> Notebook
+                                -> IO ()
+printNotebookFilteredIterate _ _ [] = putStrLn ""
+printNotebookFilteredIterate 0 f xs
+      = do
+          printNotebookHeader
+          printNotebookFilteredIterate 1 f xs
+printNotebookFilteredIterate n f (x:xs)
+      | f x       = do
+                      printEntry n x
+                      printNotebookFilteredIterate (n+1) f xs
+      | otherwise =   printNotebookFilteredIterate (n+1) f xs
+
+printNotebookFiltered :: (Entry -> Bool) -> Notebook -> IO ()
+printNotebookFiltered = printNotebookFilteredIterate 0
