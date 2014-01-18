d5dc876569ce55cca7fefd37dcefb977dd8056e6
kjrz <kuba.jarz@gmail.com>
2014-01-18 21:46:58 +0100

    changed today's entries test

--- NotebookPrinterTest.hs
+++ NotebookPrinterTest.hs
@@ -1,12 +1,13 @@
 import NotebookModel
 import NotebookPrinter
 import EventModel
+import Data.Time
 
 main :: IO ()
 main = do
 
   -- Events, entries, notebook
-  let firstDate   = eventDateTime (2000,3,3) (22,3,0)
+  let firstDate   = eventDateTime (2000,3,3) (22,3,0) :: UTCTime
   let firstEvent  = Event "w" firstDate Monthly
   let secondEvent = Event "y" (eventDateTime (2001,1,1)
                     (23,7,0)) Monthly
@@ -16,7 +17,7 @@ main = do
 
   -- Print!
   printNotebook notebook
-  printNotebookFiltered (isToday firstDate) notebook
+  printNotebookFiltered ((isForToday . utctDay) firstDate) notebook
   printNotebookFiltered (isDone True) notebook
 
   putStrLn "-- ok"
