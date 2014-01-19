8e1830b5f2e0236b8113a7dff696bf450341d8a5
kjrz <kuba.jarz@gmail.com>
2014-01-19 13:59:15 +0100

    added checks as in Menu

--- Today.hs
+++ Today.hs
@@ -5,6 +5,7 @@ module Today
   ) where
 
 import qualified Data.Map as Map
+import Data.Char
 import Data.Time
 import Data.Maybe
 
@@ -15,28 +16,24 @@ data Step = YearStep | MonthStep | DayStep | DefinitiveStep
 
 askWhatDayItIs :: Step -> Map.Map String String -> IO Day
 
-askWhatDayItIs YearStep _ = do
+askWhatDayItIs YearStep params = do
   putStrLn ""
   putStrLn "Gimme the year:"
-  yearString <- getLine
-  let year = read yearString :: Integer
-  if year > 2013
+  year <- getLine
+  if inputCorrect YearStep year
     then
-      askWhatDayItIs MonthStep
-                      $ Map.insert "year" yearString Map.empty
+      askWhatDayItIs MonthStep $ Map.insert "year" year params
     else do
       putStrLn "Stop living in the past, mate!"
       askWhatDayItIs YearStep Map.empty
 
 askWhatDayItIs MonthStep params = do
   putStrLn ""
-  putStrLn "Gimme the month:"
-  monthString <- getLine
-  let month = read monthString :: Int
-  if month > 0 && month < 13
+  putStrLn "Gimme the month (1-12):"
+  month <- getLine
+  if inputCorrect MonthStep month
     then
-      askWhatDayItIs DayStep
-                     $ Map.insert "month" monthString params
+      askWhatDayItIs DayStep $ Map.insert "month" month params
     else do
       putStrLn "Learn your months, mate!"
       askWhatDayItIs MonthStep params
@@ -44,16 +41,13 @@ askWhatDayItIs MonthStep params = do
 askWhatDayItIs DayStep params = do
   putStrLn ""
   putStrLn "Gimme the day:"
-  monthString <- getLine
-  let day = read monthString :: Int
-  let maxDay = maxDayForMonth params
-  if day > 0 && day <= maxDay
+  day <- getLine
+  if dayCorrect params day
     then
       askWhatDayItIs DefinitiveStep
-                     $ Map.insert "day" monthString params
+                     $ Map.insert "day" day params
     else do
-      putStrLn $ "The month has " ++ show maxDay ++ " days."
-      putStrLn "So learn your days, mate!"
+      putStrLn "Learn your days, mate!"
       askWhatDayItIs DayStep params
 
 askWhatDayItIs DefinitiveStep params = do
@@ -62,10 +56,26 @@ askWhatDayItIs DefinitiveStep params = do
   let day = read $ fromJust $ Map.lookup "day" params :: Int
   return $ fromGregorian year month day
 
--- One utility
+-- User input checks
+
+dayCorrect :: Map.Map String String -> String -> Bool
+dayCorrect params y
+    | not $ all isNumber y = False
+    | otherwise            = number > 0 && number <= maxDay
+    where number = read y :: Int
+          maxDay = maxDayForMonth params
 
 maxDayForMonth :: Map.Map String String -> Int
 maxDayForMonth params = do
   let year = read $ fromJust $ Map.lookup "year" params :: Integer
   let month = read $ fromJust $ Map.lookup "month" params :: Int
   gregorianMonthLength year month
+
+inputCorrect :: Step -> String -> Bool
+
+inputCorrect YearStep y = all isNumber y
+
+inputCorrect MonthStep m
+    | not $ all isNumber m = False
+    | otherwise            = number >= 1 && number <= 13
+    where number = read m :: Int
